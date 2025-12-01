87b9195b7cac0845395f802f6879efc4d22987a1
Kirk Longuski <longuskikirk@gmail.com>
2025-12-01 12:17:55 -0500

    feat(hero): add hero component with header summary and layout

---
+++ src/components/TheHero/TheHero.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import { useResumeData } from '../../composables/useResumeData'
+import { computed } from 'vue'
+import TheSummary from '../TheSummary/TheSummary.vue'
+
+const resumeData = useResumeData()
+const { basics } = resumeData.resume.value
+const links = computed(() => {
+    return Object.entries(basics?.links ?? {}).filter(([_, link]) => Boolean(link))
+})
+const telHref = computed(() => {
+    return `tel:${basics.phone?.replace(/[^0-9]/g, '')}`
+})
+</script>
+<template>
+    <div class="the-hero">
+        <h1>{{ basics.name }}</h1>
+        <div class="contact-info">
+            <a :href="telHref">{{ basics.phone }}</a>
+            <a :href="`mailto:${basics.email}`">{{ basics.email }}</a>
+            <a
+                v-for="[linkKey, link] in links"
+                :key="linkKey"
+                :href="link"
+                target="_blank"
+                class="contact-info-link"
+            >
+                {{ linkKey }}
+            </a>
+        </div>
+        <TheSummary />
+    </div>
+</template>
+<style scoped>
+.the-hero {
+    width: 100%;
+    max-width: var(--app-max-width);
+    margin: 0 auto;
+}
+.the-hero h1 {
+    font-size: 2rem;
+    font-weight: 600;
+    text-align: center;
+    margin-bottom: 10px;
+    font-family: 'IBM Plex Mono', ui-monospace, SFMono-Regular, Menlo, Monaco, 'Liberation Mono', 'Courier New', monospace;
+    padding-left: 10px;
+}
+.contact-info {
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+    padding-left: 10px;
+}
+.contact-info a {
+    font-size: .8rem;
+    margin-bottom: 10px;
+    color: var(--text-color);
+}
+</style>
