9f30cc043ff04cb9101bf6d9be76d31672d2e495
Kirk Longuski <longuskikirk@gmail.com>
2025-12-01 22:21:33 -0500

    chore: remove unused symbols and imports

--- app/components/TheExperience/ExperienceBlock/ExperienceHeader/ExperienceHeader.vue
+++ app/components/TheExperience/ExperienceBlock/ExperienceHeader/ExperienceHeader.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
-import { inject } from 'vue'
-import { currentExperienceSymbol } from '@/symbols'
 import ExperienceSubtitle from './ExperienceSubtitle/ExperienceSubtitle.vue'
 import ExperienceTitle from './ExperienceTitle/ExperienceTitle.vue'
-import { WorkExperience } from '@/types'
 </script>
 <template>
   <div class="experience-header">
